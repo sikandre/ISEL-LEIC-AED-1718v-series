8c98dad0bf4db9b2bcbc47c8c1e826c3b9083638
Diogo <diogomsbaptista@gmail.com>
2018-04-19 22:01:08 +0100

    jkk

--- src/series/serie2/ListUtils.java
+++ src/series/serie2/ListUtils.java
@@ -1,4 +1,30 @@
 package series.serie2;
 
-public class ListUtils {
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListUtils<E> {
+    static class Node<E>{
+        E value;
+        Node<E> next;
+        Node<E> previous;
+    }
+    private Node<E> head;
+    public ListUtils() {
+        head=new Node<E>();
+        head.next=head;
+        head.previous=head;
+    }
+
+    public static <E> void removeAfterIntersectionPoint(Node<E> list1,Node<E> list2,Comparator<E> cmp){
+        List<Integer> List= new LinkedList<>();
+        Node<E> current= list1.next;
+        int aux=0;
+        while (current!=head){
+            if(current.value==cmp)List.add(aux);
+            ++aux;
+            current=current.next;
+        }
+    }
 }
