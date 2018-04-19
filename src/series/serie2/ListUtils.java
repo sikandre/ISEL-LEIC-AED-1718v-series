1d8f109924b7dd9dff8920e80ea3d844dfd4cf12
Diogo <diogomsbaptista@gmail.com>
2018-04-19 22:02:19 +0100

    jkk

--- src/series/serie2/ListUtils.java
+++ src/series/serie2/ListUtils.java
@@ -16,7 +16,7 @@ public class ListUtils<E> {
         head.next=head;
         head.previous=head;
     }
-
+//k
     public static <E> void removeAfterIntersectionPoint(Node<E> list1,Node<E> list2,Comparator<E> cmp){
         List<Integer> List= new LinkedList<>();
         Node<E> current= list1.next;
