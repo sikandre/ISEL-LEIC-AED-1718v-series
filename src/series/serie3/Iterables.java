2148b9d3ea0a18d92f31765c0ce3514b1c03b873
sik_andre <sik.andre@gmail.com>
2018-05-22 19:28:58 +0100

    1.2 done

--- src/series/serie3/Iterables.java
+++ src/series/serie3/Iterables.java
@@ -1,7 +1,10 @@
 package series.serie3;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+
+
 
 public class Iterables {
     public static Iterable<Integer> alternateEvenOdd(Iterable<Integer> src){
@@ -11,10 +14,10 @@ public class Iterables {
                 return new Iterator<Integer>() {
                     Iterator<Integer> it=src.iterator();
                     Integer current =null;
+                    boolean odd = true;
                     @Override
                     public boolean hasNext() {
                         if(current !=null)return true;
-                        boolean odd=true;
                         while (it.hasNext()){
                             Integer aux =it.next();
                             if(aux%2==1 && odd){
@@ -35,6 +38,53 @@ public class Iterables {
                     public Integer next() {
                         if(!hasNext())throw new NoSuchElementException();
                         Integer aux=current;
+                        current=null;
+                        return aux;
+                    }
+                };
+            }
+        };
+    }
+
+    private static final Comparator<Integer> cmp = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1-o2;
+        }
+    } ;
+    static <E> Iterable<E> getTheIncreasingSequence(Iterable<E> sequence, Comparator<E> cmp){
+        return new Iterable<E>() {
+            @Override
+            public Iterator<E> iterator() {
+                return new Iterator<E>() {
+                    private E first;
+                    private E last;
+                    Iterator<E> it=sequence.iterator();
+                    E current = null;
+                    @Override
+                    public boolean hasNext() {
+                        if(current !=null)return true;
+                        while (it.hasNext()){
+                            E aux = it.next();
+                            if(first==null) {
+                                first = aux;
+                                last = first;
+                                current = last;
+                                return true;
+                            }
+                            if(cmp.compare(aux,last)>0){
+                               last = aux;
+                               current = last;
+                               return true;
+                            }
+                        }
+                        return false;
+                    }
+
+                    @Override
+                    public E next() {
+                        if(!hasNext())throw new NoSuchElementException();
+                        E aux=current;
                         current=null;
                         return aux;
                     }
--- tests/series/serie3/IterablesTest.java
+++ tests/series/serie3/IterablesTest.java
@@ -16,4 +16,11 @@ public class IterablesTest {
         Assertions.assertIterableEquals(arrayList1,Iterables.alternateEvenOdd(arrayList));
 
     }
+
+    @Test
+    public void getTheIncreasingSequence1(){
+        ArrayList<Integer> arrayList=new  ArrayList<Integer>(Arrays.asList(2,1,2,5,6,6,0,0,4,4,2,2,6,6,6,6,8,8));
+        ArrayList<Integer> arrayList1=new  ArrayList<Integer>(Arrays.asList(2,5,6,8));
+        Assertions.assertIterableEquals(arrayList1,Iterables.getTheIncreasingSequence(arrayList, Integer::compareTo));
+    }
 }
