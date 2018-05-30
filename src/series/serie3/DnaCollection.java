416b24d61533b5773018d8cbe756145deb5d2387
Diogo <diogomsbaptista@gmail.com>
2018-05-30 20:56:33 +0100

    fiz uma versao do 3

---
+++ src/series/serie3/DnaCollection.java
@@ -0,0 +1,66 @@
+package series.serie3;
+
+public class DnaCollection{
+    private String defaultDnaCharaters="ACTG";
+    private DNAStucture root=null;
+
+    public class DNAStucture{
+        DNAStucture parent ;
+        DNAStucture []collection=new DNAStucture[defaultDnaCharaters.length()];
+        boolean prefix=false;
+
+
+        public void addDNAStucture(DNAStucture parent,DNAStucture add,int index){
+            this.parent=parent;
+            collection[index]=add;
+        }
+    }
+
+    public DnaCollection(){
+        root =new DNAStucture();
+    }
+
+    public void add(String fragment){
+        char aux;
+        int i=0;
+        DNAStucture current=root;
+        while(i<fragment.length()){
+            aux=fragment.charAt(i);
+            int index=defaultDnaCharaters.indexOf(aux);
+            if(index>=0){
+                if(root.collection[index]==null){
+                    DNAStucture newStruct=new DNAStucture();
+                    current.addDNAStucture(current,newStruct,index);
+                    current=newStruct;
+                }
+            }else{
+                break;
+            }
+            ++i;
+        }
+        if(i==fragment.length())
+            current.prefix=true;
+    }
+    public int prefixCount(String p){
+        char aux;
+        int i=0;
+        int counter=0;
+        DNAStucture current=root;
+        while(i<p.length()){
+            aux=p.charAt(i);
+            int index=defaultDnaCharaters.indexOf(aux);
+            if(index>=0)
+                current=root.collection[index];
+            else
+                return -1;
+            ++i;
+        }
+        while(current.parent!=null){
+            for(DNAStucture st:current.collection)
+                if(st.prefix)
+                    ++counter;
+            current=current.parent;
+        }
+        return counter;
+    }
+}
